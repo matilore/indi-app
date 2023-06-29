cd6f45b371554c5b18be60a18733ff2087e8abf3
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-06-29 15:58:12 +0200

    [RED] retuning data from useGetPodcasts hook

--- src/interface/pages/Home/hooks/useGetPodcasts.test.tsx
+++ src/interface/pages/Home/hooks/useGetPodcasts.test.tsx
@@ -1,8 +1,10 @@
 
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { getPodcastsResponse } from '../../../../application/useCases/getPodcasts';
 import { PodcastRepositoryImpl } from '../../../../infrastructure/api/podcastRepositoryImpl';
 import { useGetPodcasts } from './useGetPodcasts';
+import {MOCKED_RESPONSE} from '../../../../infrastructure/api/__mocks__/podcastRepositoryImpl'
+
 
 vi.mock("../../../../infrastructure/api/podcastRepositoryImpl")
 
@@ -13,6 +15,18 @@ describe('useGetPodcasts', () => {
         const podcastRepository = new PodcastRepositoryImpl()
         renderHook(() => useGetPodcasts(getPodcastsResponse, podcastRepository));
         expect(podcastRepository.getAllPodcasts).toHaveBeenCalled()
+
+    })
+
+
+    it('should return a list of podcasts', async () => {
+
+        const podcastRepository = new PodcastRepositoryImpl()
+        const {result } = renderHook(() => useGetPodcasts(getPodcastsResponse, podcastRepository));
+
+        await waitFor(() => {
+            expect(result.current.podcasts).toEqual(MOCKED_RESPONSE);
+          });
 
     })
 })
