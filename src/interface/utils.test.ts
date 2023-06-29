5d82deb67c3399b4bd51f133520b474a8f467ad9
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-06-29 17:58:59 +0200

    [RED] get local storage data

--- src/interface/utils.test.ts
+++ src/interface/utils.test.ts
@@ -1,10 +1,25 @@
-import {checkDataExpiration} from './utils'
+import {getLocalStorageData, checkDataExpiration} from './utils'
 
 
 describe('checkDataExpiration', () => {
     it('should check if local storage data lifespam is expired', () => {
-
-       const  isExpire = checkDataExpiration({localStorageExpiration: 9, currentTime: Date.now()})
+       const isExpire = checkDataExpiration({localStorageExpiration: 9, currentTime: 10})
        expect(isExpire).toBeTruthy()
+    })
+})
+
+
+describe('getLocalStorageData', () => {
+
+    it('should return a list of podcasts if data is present and not expired', () => {
+
+        const stringifiedData = '{expirationDate: 10, data: [{key: value}]}'
+        localStorage.setItem('podcasts', stringifiedData)
+
+        const podcasts = getLocalStorageData()
+
+        expect(podcasts).toMatchObject({expirationDate: 10, data: [{key: value}]})
+
+   
     })
 })
--- src/interface/utils.ts
+++ src/interface/utils.ts
@@ -1,5 +1,10 @@
 export const setExpirationLocalStorageItem = (hours: number) =>  new Date().getTime() + hours * 60 * 60 * 1000;
 
-export const checkDataExpiration = ({localStorageExpiration, currentTime = Date.now()}: {localStorageExpiration: number,currentTime: number }) => {
+export const checkDataExpiration = ({localStorageExpiration, currentTime = Date.now()}: {localStorageExpiration: number, currentTime?: number }) => {
     return localStorageExpiration < currentTime;
+}
+
+
+export const getLocalStorageData = () => {
+    
 }
