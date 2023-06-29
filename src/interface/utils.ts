f46ae72e4f703838b5fe7234654a54b0deccfca1
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-06-29 16:42:54 +0200

    [GREEN] realize expiration data check

--- src/interface/utils.ts
+++ src/interface/utils.ts
@@ -1,5 +1,5 @@
 export const setExpirationLocalStorageItem = (hours: number) =>  new Date().getTime() + hours * 60 * 60 * 1000;
 
-export const checkDataExpiration = () => {
-
+export const checkDataExpiration = ({localStorageExpiration, currentTime = Date.now()}: {localStorageExpiration: number,currentTime: number }) => {
+    return localStorageExpiration < currentTime;
 }
