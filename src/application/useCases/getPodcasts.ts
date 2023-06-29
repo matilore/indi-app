d8b041126f488360498378eeb2436363f298f1e2
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-06-29 12:05:57 +0200

    [GREEN] implement getPodcastsResponse use case

--- src/application/useCases/getPodcasts.ts
+++ src/application/useCases/getPodcasts.ts
@@ -1,4 +1,7 @@
+import { PodcastRepository } from "../../domain/interfaces";
 
 export const getPodcastsResponse = async (
+  podcastRepository: PodcastRepository
 ) => {
+  return await podcastRepository.getAllPodcasts();
 };
