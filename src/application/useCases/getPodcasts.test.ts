0efc3e23543c3680d3d66de90256c9879d0c26b0
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-06-29 12:04:23 +0200

    [RED] add getPodcastsResponse use case

---
+++ src/application/useCases/getPodcasts.test.ts
@@ -0,0 +1,12 @@
+import { vi } from 'vitest';
+import { PodcastRepositoryImpl } from '../../infrastructure/api/podcastRepositoryImpl';
+import {getPodcastsResponse} from './getPodcasts'
+vi.mock('../../infrastructure/api/podcastRepositoryImpl')
+
+describe('getPodcastsResponse', () => {
+  it('should call getAllPodcasts method of PodcastRepositoryImpl instance', async () => {
+     const podcastRepository = new PodcastRepositoryImpl()
+     getPodcastsResponse(podcastRepository)
+     expect(podcastRepository.getAllPodcasts).toHaveBeenCalledTimes(1)
+  });
+});
