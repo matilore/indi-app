7d95d62518cba2efb999214ab33fe62e13895d41
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-07-01 11:21:12 +0200

    [RED] add podcasts data after fetch

--- src/interface/pages/Home/hooks/useGetPodcasts.test.tsx
+++ src/interface/pages/Home/hooks/useGetPodcasts.test.tsx
@@ -1,10 +1,10 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import { getPodcastsResponse } from "@/application/useCases/getPodcasts";
 import { PodcastRepositoryImpl } from "@/infrastructure/api/podcastRepositoryImpl";
 import { useGetPodcasts } from "./useGetPodcasts";
 import { MOCKED_RESPONSE } from "@/infrastructure/api/constants";
 import { LOCAL_STORAGE_ITEM_NAME } from "../constants";
-
+import { vi } from "vitest";
 vi.mock("@/infrastructure/api/podcastRepositoryImpl");
 
 describe("useGetPodcasts", () => {
@@ -48,6 +48,17 @@ describe("useGetPodcasts", () => {
 
     await waitFor(() => {
       expect(result.current.podcasts).toEqual(MOCKED_RESPONSE);
+    });
+  });
+
+  it("should store the list of podcasts", async () => {
+    const spied = vi.spyOn(Storage.prototype, "setItem");
+    const podcastRepository = new PodcastRepositoryImpl();
+    renderHook(() => useGetPodcasts(getPodcastsResponse, podcastRepository));
+
+    await waitFor(() => {
+      expect(podcastRepository.getAllPodcasts).toHaveBeenCalled();
+      expect(spied).toHaveBeenCalled();
     });
   });
 });
