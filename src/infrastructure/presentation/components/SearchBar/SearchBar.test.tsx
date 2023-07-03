02b6d5ebdace12ffffb9c5e9203e83bfd3aaafc3
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-07-03 18:29:30 +0200

    [RED] add podcast number to badge

--- src/infrastructure/presentation/components/SearchBar/SearchBar.test.tsx
+++ src/infrastructure/presentation/components/SearchBar/SearchBar.test.tsx
@@ -1,6 +1,9 @@
 import { SearchBar } from ".";
 import { render } from "@testing-library/react";
 import { SEARCH_BAR_PLACEHOLDER, PODCAST_NUMBER_BADGE_ID } from "./constants";
+import { MOCKED_RESPONSE } from "@/infrastructure/api/constants";
+
+vi.mock("@/infrastructure/api/podcastRepositoryImpl");
 
 describe("SearchBar", () => {
   it("should render an input field with a specific placeholder", () => {
@@ -12,6 +15,6 @@ describe("SearchBar", () => {
   it("should display a badge with the number of filtered podcasts", () => {
     const { getByTestId } = render(<SearchBar />);
     const badge = getByTestId(PODCAST_NUMBER_BADGE_ID);
-    expect(badge).toBeDefined();
+    expect(badge.textContent).toEqual(MOCKED_RESPONSE.length);
   });
 });
