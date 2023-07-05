eff0874d05183b5b1e1013bf4b336c36e50b1021
Matias Lorenzo <matias.nahuel.lorenzo@gmail.com>
2023-07-05 10:22:01 +0200

    [RED] add spinner in header between transitions

--- src/infrastructure/presentation/components/Header/Header.test.tsx
+++ src/infrastructure/presentation/components/Header/Header.test.tsx
@@ -27,4 +27,28 @@ describe("Header", () => {
     const linkHref = link.getAttribute("href");
     expect(linkHref).toBe(ROUTES_PATHS.HOME);
   });
+
+  it("should render a spinner when isPending is true", () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Header isPending={true} />
+      </MemoryRouter>
+    );
+
+    const spinner = getByTestId("spinner");
+
+    expect(spinner).toBeDefined();
+  });
+
+  it("should not render spinner when isPending is false", () => {
+    const { queryByTestId } = render(
+      <MemoryRouter>
+        <Header isPending={false} />
+      </MemoryRouter>
+    );
+
+    const spinner = queryByTestId("spinner");
+
+    expect(spinner).toBeNull();
+  });
 });
